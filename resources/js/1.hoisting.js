f742ac3c1e42f4505aa489ad356403f661d07ee5
Hyun7en <rhqudgus99@gmail.com>
2024-03-22 09:07:52 +0900

    JS

---
+++ resources/js/1.hoisting.js
@@ -0,0 +1,20 @@
+/**
+ * Hoisting
+ */
+
+console.log(name); // name
+var name = "병현";
+console.log(name);
+
+/**
+ * Hoisting은 뭘까?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+ * let, const
+*/
+
+console.log(name2);
+let name2= "junIk";
+
+
+
---
+++ resources/js/2.scope.js
@@ -0,0 +1,58 @@
+/**
+ * Scope
+ * 
+ */
+
+var number1 = 20;
+
+function test1(){
+    console.log(number1);
+
+}
+
+// test1();
+
+function test2(){
+    var number1 = 40;
+    console.log(number1);
+}
+
+// test2();
+
+var number1 = 20;
+
+function test3(){
+    var number1 = 40;
+    test4();
+    console.log("number1 : " + number1);
+}
+
+function test4(){
+    var number2 = 11;
+    console.log("number2 : " + number2);
+    console.log("number1 : " + number1);
+}
+
+test3();
+console.log(number1);
+
+/**
+ *  JS => Lexical Scope
+ * 선언된 위치가 상위 스코프를 정한다.
+ * 
+ * Dynamic Scope
+ * 실행한 위치가 상위 스코프를 정한다
+ * 
+ */
+
+// var i = 1000;
+// for(var i = 0; i< 10; i++){
+//     console.log(i);
+// }
+// console.log("i = " + i);
+
+let i = 1000;
+for(let i = 0; i< 10; i++){
+    console.log(i);
+}
+console.log("i = " + i);
